1f0bc75c672b41041dc2e0cb7c18aaf015114fc7
Joseph Manley <j@cloudsumu.com>
2019-11-22 15:30:58 -0500

    Override bumpversion

--- semver/get_version.py
+++ semver/get_version.py
@@ -11,18 +11,17 @@ except ImportError:
     import os
     DEVNULL = open(os.devnull, 'wb')
 
-
-def get_version(dot=False):
-    config = ConfigParser()
-    config.read('./.bumpversion.cfg')
-
+def get_tag_version():
     version = "develop"
     
     tagged_versions = subprocess.Popen(['git','tag','-l','[0-9]*.[0-9]*.[0-9]*'],
         stdout=subprocess.PIPE, stderr=DEVNULL, cwd=".").stdout.read().decode('utf-8').rstrip().split('\n')
     if len(tagged_versions) > 0 and tagged_versions[-1] != "":
         version = tagged_versions[-1]
-    
+    return version
+
+def get_version(dot=False):
+    version = get_tag_version()
 
     # Get the commit hash of the version 
     v_hash = subprocess.Popen(['git', 'rev-list', '-n', '1', version], stdout=subprocess.PIPE,
