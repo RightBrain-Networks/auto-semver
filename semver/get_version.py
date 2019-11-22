7be7706acca663938c160ec40f1c88cd4c85f1e1
Joseph Manley <j@cloudsumu.com>
2019-11-22 16:57:30 -0500

    Grab tagged version based on .bumpversion.cfg

--- semver/get_version.py
+++ semver/get_version.py
@@ -12,9 +12,13 @@ except ImportError:
     DEVNULL = open(os.devnull, 'wb')
 
 def get_tag_version():
+    config = ConfigParser()
+    config.read('./.bumpversion.cfg')
+    tag_expression = config.get('bumpversion','tag_name').replace('{new_version}','[0-9]*.[0-9]*.[0-9]*')
+
     version = "0.0.0"
     
-    tagged_versions = subprocess.Popen(['git','tag','-l','[0-9]*.[0-9]*.[0-9]*'],
+    tagged_versions = subprocess.Popen(['git','tag','-l',tag_expression],
         stdout=subprocess.PIPE, stderr=DEVNULL, cwd=".").stdout.read().decode('utf-8').rstrip().split('\n')
     if len(tagged_versions) > 0 and tagged_versions[-1] != "":
         version = tagged_versions[-1]
