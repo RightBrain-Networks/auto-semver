085b20a10fd64941ffb4d6d5954ca5409604f529
Joseph Manley <j@cloudsumu.com>
2020-01-31 13:14:26 -0500

    Added getVersion() groovy function

---
+++ vars/getVersion.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/env groovy
+
+/** 
+ * Run semver_get_version to return the current version of the repository
+ *
+ * @param flags Flags or arguments to pass to semver_get_version
+ * @return "1.9.2"
+ */
+def call(flags='') {
+    def VERSION
+    VERSION = sh(returnStdout: true, script: "semver_get_version ${flags}")
+    return VERSION.trim()
+}
+
