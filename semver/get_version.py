74fe2a324d1ce109ecce3896960bc210e5a1d04a
Derek DeJonghe <derek.dejonghe@rightbrainnetworks.com>
2020-06-15 17:29:40 -0400

    use output not error

--- semver/get_version.py
+++ semver/get_version.py
@@ -25,7 +25,7 @@ def get_version(build=0,version_format=None,dot=False):
         semver.merged_branch = branch
         version_type = semver.get_version_type()
         p = subprocess.Popen(['bumpversion', '--dry-run', '--verbose', '--current-version', get_tag_version(), version_type], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd='.')
-        bump_output = p.stderr.read().decode()
+        bump_output = p.stdout.read().decode('utf-8').rstrip()
         next_version = re.search("new_version=([0-9]*.[0-9]*.[0-9]*)", bump_output).group(1)
 
         if version_format == 'npm':
